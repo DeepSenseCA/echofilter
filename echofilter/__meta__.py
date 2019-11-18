4cf706fc14cfc5ae0b4e94c3297d0cc344aa4b5f
Scott Lowe <scott.code.lowe@gmail.com>
2019-11-18 11:12:35 -0400

    Initialise from skeleton repository
    https://github.com/scottclowe/python-template-repo

---
+++ echofilter/__meta__.py
@@ -0,0 +1,8 @@
+name = 'echofilter'  # See https://www.python.org/dev/peps/pep-0008/
+path = name.lower().replace("-", "_").replace(" ", "_")
+version = '0.1.dev0'  # https://python.org/dev/peps/pep-0440 https://semver.org
+author = 'DeepSense'
+author_email = ''
+description = ''  # One-liner
+url = ''  # your project homepage
+license = 'Closed Source'  # See https://choosealicense.com
