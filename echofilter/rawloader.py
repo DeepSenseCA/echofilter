ba3196e909be892c249b9bfd99e888c836c96fe1
Scott Lowe <scott.code.lowe@gmail.com>
2019-12-04 20:58:15 -0400

    ENH: Add transect loader, and partition list loader

--- echofilter/rawloader.py
+++ echofilter/rawloader.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 
 
+ROOT_DATA_DIR = '/media/scott/scratch/Datasets/dsforce'
+
 TRANSECT_FIELD_TYPES = {
     'Ping_index': int,
     'Distance_gps': float,
@@ -207,3 +209,136 @@ def evl_loader(fname):
         timestamps.append(timestamp)
         values.append(value)
     return np.array(timestamps), np.array(values)
+
+
+def load_transect_data(
+        transect_pth,
+        dataset='surveyExports',
+        root_data_dir=ROOT_DATA_DIR
+        ):
+    '''
+    Load all data for one transect.
+
+    Parameters
+    ----------
+    transect_pth : str
+        Relative path to transect, excluding '_Sv_raw.csv'.
+    dataset : str, optional
+        Name of dataset. Default is `'surveyExports'`.
+    root_data_dir : str
+        Path to root directory where data is located.
+
+    Returns
+    -------
+    timestamps : numpy.ndarray
+        Timestamps (in seconds since Unix epoch), with each entry
+        corresponding to each row in the `signals` data.
+    depths : numpy.ndarray
+        Depths from the surface (in metres), with each entry corresponding
+        to each column in the `signals` data.
+    signals : numpy.ndarray
+        Echogram Sv data, shaped (num_timestamps, num_depths).
+    top : numpy.ndarray
+        Depth of top line, shaped (num_timestamps, ).
+    bottom : numpy.ndarray
+        Depth of bottom line, shaped (num_timestamps, ).
+    '''
+    dirname = os.path.join(root_data_dir, dataset)
+    raw_fname = os.path.join(dirname, transect_pth + '_Sv_raw.csv')
+    bot_fname = os.path.join(dirname, transect_pth + '_bottom.evl')
+    top_fname = os.path.join(dirname, transect_pth + '_turbulence.evl')
+
+    timestamps, depths, signals = transect_loader(raw_fname)
+    t_bot, d_bot = evl_loader(bot_fname)
+    t_top, d_top = evl_loader(top_fname)
+
+    return (
+        timestamps,
+        depths,
+        signals,
+        np.interp(timestamps, t_top, d_top),
+        np.interp(timestamps, t_bot, d_bot),
+    )
+
+
+def get_partition_data(
+        partition,
+        dataset='surveyExports',
+        partitioning_version='firstpass',
+        root_data_dir=ROOT_DATA_DIR,
+        ):
+    '''
+    Loads partition metadata.
+
+    Parameters
+    ----------
+    transect_pth : str
+        Relative path to transect, excluding '_Sv_raw.csv'.
+    dataset : str, optional
+        Name of dataset. Default is `'surveyExports'`.
+    partitioning_version : str, optional
+        Name of partitioning method.
+    root_data_dir : str
+        Path to root directory where data is located.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Metadata for all transects in the partition. Each row is a single
+        sample.
+    '''
+    dirname = os.path.join(root_data_dir, dataset, 'sets', partitioning_version)
+    fname_partition = os.path.join(dirname, partition + '.txt')
+    fname_header = os.path.join(dirname, 'header' + '.txt')
+
+    with open(fname_header, 'r') as hf:
+        for row in csv.reader(hf):
+            header = [entry.strip() for entry in row]
+            break
+
+    df = pd.read_csv(fname_partition, header=None, names=header)
+    return df
+
+
+def get_partition_list(
+        partition,
+        dataset='surveyExports',
+        full_path=False,
+        partitioning_version='firstpass',
+        root_data_dir=ROOT_DATA_DIR,
+    ):
+    '''
+    Get a list of transects in a single partition.
+
+    Parameters
+    ----------
+    transect_pth : str
+        Relative path to transect, excluding '_Sv_raw.csv'.
+    dataset : str, optional
+        Name of dataset. Default is `'surveyExports'`.
+    full_path : bool, optional
+        Whether to return the full path to the sample. If `False`, only the
+        relative path (from the dataset directory) is returned.
+        Default is `False`.
+    partitioning_version : str, optional
+        Name of partitioning method.
+    root_data_dir : str
+        Path to root directory where data is located.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Metadata for all transects in the partition. Each row is a single
+        sample.
+    '''
+    df = get_partition_data(
+        partition,
+        dataset=dataset,
+        partitioning_version=partitioning_version,
+        root_data_dir=root_data_dir,
+    )
+    fnames = df['Filename']
+    fnames = [os.path.join(f.split('_')[0], f.strip().replace('_Sv_raw.csv', '')) for f in fnames]
+    if full_path:
+        fnames = [os.path.join(root_dir, f) for f in fnames]
+    return fnames
