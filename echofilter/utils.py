ebb215519a9f2d0bc645ff6e7aefb9c7b71341dc
Scott Lowe <scott.code.lowe@gmail.com>
2020-04-17 15:39:29 +0100

    ENH: Add get_current_momentum

--- echofilter/utils.py
+++ echofilter/utils.py
@@ -59,6 +59,32 @@ def get_current_lr(optimizer):
     return optimizer.param_groups[0]['lr']
 
 
+def get_current_momentum(optimizer):
+    '''
+    Get the momentum of an optimizer.
+
+    Parameters
+    ----------
+    optimizer : torch.optim.Optimizer
+        An optimizer which implements momentum or betas (where momentum is the
+        first beta, c.f. torch.optim.Adam) with a momentum common to all
+        parameter groups.
+
+    Returns
+    -------
+    float
+        The momentum of the first parameter group.
+    '''
+    if 'momentum' not in optimizer.defaults and 'betas' not in optimizer.defaults:
+        raise ValueError('optimizer {} does not support momentum'.format(optimizer.__class__))
+
+    group = optimizer.param_groups[0]
+    if 'momentum' in group:
+        return group['momentum']
+    else:
+        return group['betas'][0]
+
+
 class TensorDict(nn.ParameterDict):
     r"""Holds tensors in a dictionary.
 
