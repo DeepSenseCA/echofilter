8fed83d480027b7635759615671117483adb73e0
Scott Lowe <scott.code.lowe@gmail.com>
2020-06-08 15:14:01 +0100

    MNT: Tidy up separate_stationary.sh
    In particular, ensure strings are strings by encapsulating them
    with quotes, as is good practice.

--- bin/separate_stationary.sh
+++ bin/separate_stationary.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
 # Take a list of dates, and separate them into test/train/validate sets
 #
 # One way to get that list would be something like:
-# for survey in december2017  march2018  september2018 ; do ls -1 $survey/evExports/*.csv ; done | sed "s:-.*::g" >dates.txt
+# for survey in december2017 march2018 september2018 ; do ls -1 $survey/evExports/*.csv ; done | sed "s:-.*::g" >dates.txt
 
-bdir="/data/dsforce/surveyExports/stationary"
 wdir="/data/dsforce/surveyExports/stationary/sets/temp"
 
 infile="dates.txt"
@@ -20,37 +18,33 @@ tempfile="temp.txt"
 shuffile="shuffle.txt"
 
 myseed="1"
-if [ $# -ge 1 ] ; then
+if [ $# -ge 1 ]; then
   myseed="$1"
 fi
 
-get_seeded_random()
-{
-  seed="$1"
-  openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt \
-    </dev/zero 2>/dev/null
+get_seeded_random() {
+    seed="$1"
+    openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt </dev/zero 2>/dev/null
 }
 
-
 total=`cat $infile | wc -l`
-testnum=`echo "scale=0;$total / 10" | bc  `
+testnum=`echo "scale=0;$total / 10" | bc`
 trainnum=`echo "scale=0;$total - ( 2 * $testnum )" | bc`
 echo "Total : $total"
 echo "Train : $trainnum"
 echo "Test  : $testnum"
 
-rm -rf $tempfile
+rm -rf "$tempfile"
 
 # shuffle, and put enough in each file
-shuf --random-source=<(get_seeded_random $myseed) $infile > $shuffile
-head -n $trainnum $shuffile > $traindates
-tail -n `echo "scale=0;$testnum * 2" | bc` $shuffile > $tempfile
-tail -n $testnum $tempfile > $valdates
-head -n $testnum $tempfile > $testdates
+shuf --random-source=<(get_seeded_random $myseed) "$infile" > "$shuffile"
+head -n $trainnum $shuffile > "$traindates"
+tail -n `echo "scale=0;$testnum * 2" | bc` "$shuffile" > "$tempfile"
+tail -n $testnum "$tempfile" > "$valdates"
+head -n $testnum "$tempfile" > "$testdates"
 
-rm -rf $tempfile
+rm -rf "$tempfile"
 
-cd $bdir
-for line in `cat $wdir/$traindates` ; do ls -1 ${line}*.csv ; done > $wdir/$trainfile
-for line in `cat $wdir/$testdates` ; do ls -1 ${line}*.csv ; done > $wdir/$testfile
-for line in `cat $wdir/$valdates` ; do ls -1 ${line}*.csv ; done > $wdir/$valfile
+for line in `cat "$wdir/$traindates"`; do ls -1 ${line}*.csv; done > "$wdir/$trainfile"
+for line in `cat "$wdir/$testdates"`; do ls -1 ${line}*.csv; done > "$wdir/$testfile"
+for line in `cat "$wdir/$valdates"`; do ls -1 ${line}*.csv; done > "$wdir/$valfile"
