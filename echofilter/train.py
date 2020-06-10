54d8c2829e7748a4b75c071d8bf9e62618ae959d
Scott Lowe <scott.code.lowe@gmail.com>
2020-06-10 11:47:06 +0100

    BUG: Don't convert dtype of targets when sending to device

--- echofilter/train.py
+++ echofilter/train.py
@@ -791,9 +791,7 @@ def train_epoch(
         data_time.update(time.time() - end)
 
         input = input.to(device, dtype, non_blocking=True)
-        metadata = {
-            k: v.to(device, dtype, non_blocking=True) for k, v in metadata.items()
-        }
+        metadata = {k: v.to(device, non_blocking=True) for k, v in metadata.items()}
 
         # Compute output
         output = model(input)
@@ -944,9 +942,7 @@ def validate(
             data_time.update(time.time() - end)
 
             input = input.to(device, dtype, non_blocking=True)
-            metadata = {
-                k: v.to(device, dtype, non_blocking=True) for k, v in metadata.items()
-            }
+            metadata = {k: v.to(device, non_blocking=True) for k, v in metadata.items()}
 
             # Compute output
             output = model(input)
