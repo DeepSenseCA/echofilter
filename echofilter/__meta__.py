022e864b2018c053b419684d7af66edf70d91b0a
Scott Lowe <scott.code.lowe@gmail.com>
2020-06-18 12:25:22 +0100

    REL: Version 1.0.0b1

--- echofilter/__meta__.py
+++ echofilter/__meta__.py
@@ -1,6 +1,6 @@
 name = "echofilter"
 path = name.lower().replace("-", "_").replace(" ", "_")
-version = "0.1.4"
+version = "1.0.0b1"
 author = "Scott C. Lowe"
 author_email = "scott.code.lowe@gmail.com"
 description = "Remove echosounder noise by identifying the ocean floor and entrained air at the ocean surface."
