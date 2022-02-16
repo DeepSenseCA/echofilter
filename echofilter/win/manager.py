0afff148d8f5933e838e7008d0325f2c66c49a2a
Scott Lowe <scott.code.lowe@gmail.com>
2022-02-16 12:18:30 +0000

    REL: Release under AGPLv3

--- echofilter/win/manager.py
+++ echofilter/win/manager.py
@@ -2,6 +2,22 @@
 Window management for Windows.
 """
 
+# This file is part of Echofilter.
+#
+# Copyright (C) 2020-2022  Scott C. Lowe and Offshore Energy Research Association (OERA)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from contextlib import contextmanager
 import re
 
