98b1a399cbda7ec7a22e71906db1ce637dfe0be9
Scott Lowe <scott.code.lowe@gmail.com>
2022-04-19 11:20:07 -0300

    MNT: Vendor a copy of torchutils.count_parameters

--- echofilter/nn/utils.py
+++ echofilter/nn/utils.py
@@ -167,3 +167,22 @@ class TensorDict(torch.nn.ParameterDict):
             child_lines.append("  (" + k + "): " + parastr)
         tmpstr = "\n".join(child_lines)
         return tmpstr
+
+
+def count_parameters(model, only_trainable=True):
+    r"""
+    Count the number of (trainable) parameters within a model and its children.
+
+    Arguments:
+        model (torch.nn.Model): the model.
+        only_trainable (bool, optional): indicates whether the count should be restricted
+            to only trainable parameters (ones which require grad), otherwise all
+            parameters are included. Default is ``True``.
+
+    Returns:
+        int: total number of (trainable) parameters possessed by the model.
+    """
+    if only_trainable:
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    else:
+        return sum(p.numel() for p in model.parameters())
