c97ab049aaa54737304655343bd7a5857d803088
Scott Lowe <scott.code.lowe@gmail.com>
2022-11-06 11:52:04 -0500

    MNT: Need to change from removed np.asscalar to .item()
    This is due to numpy deprecating asscalar.

--- echofilter/utils.py
+++ echofilter/utils.py
@@ -45,7 +45,7 @@ def mode(a, axis=None, keepdims=False, **kwargs):
         return m
     m = m.squeeze(0 if axis is None else axis)
     if m.size == 1:
-        m = np.asscalar(m)
+        m = m.item()
     return m
 
 
