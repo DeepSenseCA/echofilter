778fe286ea8b8f5d4101a74d22fb543837b678a9
Scott Lowe <scott.code.lowe@gmail.com>
2022-11-17 13:12:54 -0500

    DEV: Add a parent dir to PATH in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 
 from echofilter import __meta__ as meta  # noqa: E402
